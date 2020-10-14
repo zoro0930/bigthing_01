8059ac8c627d8170e46e4a3369abff3b4cb2e5f9
索隆 <zoro.cn>
2020-10-14 11:31:56 +0800

    新增了user***.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,22 +1,25 @@
-$(function () {
+$(function() {
   // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function () {
+  $('#link_reg').on('click', function() {
     $('.login-box').hide()
     $('.reg-box').show()
   })
+
   // 点击“去登录”的链接
-  $('#link_login').on('click', function () {
+  $('#link_login').on('click', function() {
     $('.login-box').show()
     $('.reg-box').hide()
   })
+
   // 从 layui 中获取 form 对象
   var form = layui.form
-  //通过form.verify（）函数自定义校验规则
+  var layer = layui.layer
+  // 通过 form.verify() 函数自定义校验规则
   form.verify({
-    //自定义了一个叫做 pwd 校验
+    // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    //
-    repwd: function (value) {
+    // 校验两次密码是否一致的规则
+    repwd: function(value) {
       // 通过形参拿到的是确认密码框中的内容
       // 还需要拿到密码框中的内容
       // 然后进行一次等于的判断
@@ -29,7 +32,7 @@ $(function () {
   })
 
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function (e) {
+  $('#form_reg').on('submit', function(e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
     // 2. 发起Ajax的POST请求
@@ -37,7 +40,7 @@ $(function () {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function (res) {
+    $.post('/api/reguser', data, function(res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
@@ -48,7 +51,7 @@ $(function () {
   })
 
   // 监听登录表单的提交事件
-  $('#form_login').submit(function (e) {
+  $('#form_login').submit(function(e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
@@ -56,7 +59,7 @@ $(function () {
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function (res) {
+      success: function(res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
@@ -69,4 +72,3 @@ $(function () {
     })
   })
 })
-
